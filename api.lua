2ca56483c63ebe0aa5888848949dfc2325b55edb
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2019-03-04 22:34:15 +0100

    Modify move ls() into api file

--- api.lua
+++ api.lua
@@ -24,4 +24,49 @@ function api.folder()
 	love.system.openURL('file://'..love.filesystem.getWorkingDirectory())
 end
 
+function api.ls()
+	local files = love.filesystem.getDirectoryItems(currentDirectory)
+	api.print('directory: '..currentDirectory, nil, nil, 12)
+	local output = {}
+	for _, file in ipairs(files) do
+		if love.filesystem.isDirectory(currentDirectory..file) then
+			output[#output+1] = {name=file:lower(), color=14}
+		end
+	end
+	for _, file in ipairs(files) do
+		if love.filesystem.isDirectory(currentDirectory..file) then
+		elseif file:sub(-3) == '.p8' or file:sub(-4) == '.png' then
+			output[#output+1] = {name=file:lower(), color=6}
+		else
+			output[#output+1] = {name=file:lower(), color=5}
+		end
+	end
+	local count = 0
+	love.keyboard.setTextInput(false)
+	for i, item in ipairs(output) do
+		api.color(item.color)
+		for j=1,#item.name,32 do
+			api.print(item.name:sub(j,j+32))
+			flip_screen()
+			count = count + 1
+			if count == 20 then
+				api.print('--more--', nil, nil, 12)
+				flip_screen()
+				local y = _getcursory() - 6
+				api.cursor(0, y)
+				api.rectfill(0, y, 127, y+6, 0)
+				api.color(item.color)
+				while true do
+					local e = love.event.wait()
+					if e == 'keypressed' then
+						break
+					end
+				end
+				count = 0
+			end
+		end
+	end
+	love.keyboard.setTextInput(true)
+end
+
 return api
--- main.lua
+++ main.lua
@@ -81,7 +81,7 @@ local __sample_rate = 22050
 local channels = 1
 local bits = 16
 
-local currentDirectory = '/'
+currentDirectory = '/'
 local fontchars = 'abcdefghijklmnopqrstuvwxyz"\'`-_/1234567890!?[](){}.,;:<>+=%#^*~ '
 
 function shdr_unpack(tbl)
@@ -1628,51 +1628,6 @@ function _load(_cartname)
 	if load_p8(currentDirectory.._cartname) then
 		api.print('loaded '.._cartname, nil, nil, 6)
 	end
-end
-
-function api.ls()
-	local files = love.filesystem.getDirectoryItems(currentDirectory)
-	api.print('directory: '..currentDirectory, nil, nil, 12)
-	local output = {}
-	for _, file in ipairs(files) do
-		if love.filesystem.isDirectory(currentDirectory..file) then
-			output[#output+1] = {name=file:lower(), color=14}
-		end
-	end
-	for _, file in ipairs(files) do
-		if love.filesystem.isDirectory(currentDirectory..file) then
-		elseif file:sub(-3) == '.p8' or file:sub(-4) == '.png' then
-			output[#output+1] = {name=file:lower(), color=6}
-		else
-			output[#output+1] = {name=file:lower(), color=5}
-		end
-	end
-	local count = 0
-	love.keyboard.setTextInput(false)
-	for i, item in ipairs(output) do
-		api.color(item.color)
-		for j=1,#item.name,32 do
-			api.print(item.name:sub(j,j+32))
-			flip_screen()
-			count = count + 1
-			if count == 20 then
-				api.print('--more--', nil, nil, 12)
-				flip_screen()
-				local y = _getcursory() - 6
-				api.cursor(0, y)
-				api.rectfill(0, y, 127, y+6, 0)
-				api.color(item.color)
-				while true do
-					local e = love.event.wait()
-					if e == 'keypressed' then
-						break
-					end
-				end
-				count = 0
-			end
-		end
-	end
-	love.keyboard.setTextInput(true)
 end
 
 function api.cd(name)

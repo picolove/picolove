1c0e180ea1b81f9b1515312e000703f40f6f9141
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2019-03-08 23:46:13 +0100

    Modify move btn() into api file

--- main.lua
+++ main.lua
@@ -1439,30 +1439,6 @@ __keymap = {
 	}
 }
 
-function api.btn(i,p)
-	if type(i) == 'number' then
-		p = p or 0
-		if __keymap[p] and __keymap[p][i] then
-			return __pico_keypressed[p][i] ~= nil
-		end
-		return false
-	else
-		-- return bitfield of buttons
-		local bitfield = 0
-		for i=0,7 do
-			if __pico_keypressed[0][i] then
-				bitfield = bitfield + bit.lshift(1,i)
-			end
-		end
-		for i=0,7 do
-			if __pico_keypressed[1][i] then
-				bitfield = bitfield + bit.lshift(1,i+8)
-			end
-		end
-		return bitfield
-	end
-end
-
 function api.btnp(i,p)
 	if type(i) == 'number' then
 		p = p or 0

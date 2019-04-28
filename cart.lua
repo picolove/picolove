ab68fc08f0d852b3d807698acb91e9b8e772c8eb
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2019-04-28 17:17:46 +0200

    Modify simplify musicdata loading

--- cart.lua
+++ cart.lua
@@ -369,13 +369,7 @@ function cart.load_p8(filename)
 		if musicdata then
 			local _music = 0
 
-			local next_line = 1
-			while next_line do
-				local end_of_line = musicdata:find("\n",next_line)
-				if end_of_line == nil then break end
-				end_of_line = end_of_line - 1
-				local line = musicdata:sub(next_line,end_of_line)
-
+			for line in musicdata:gmatch("(.-)\n") do
 				pico8.music[_music] = {
 					loop = tonumber(line:sub(1,2),16),
 					[0] = tonumber(line:sub(4,5),16),
@@ -384,7 +378,6 @@ function cart.load_p8(filename)
 					[3] = tonumber(line:sub(10,11),16)
 				}
 				_music = _music + 1
-				next_line = musicdata:find("\n",end_of_line)+1
 			end
 		end
 	end

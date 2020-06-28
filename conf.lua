0fce7bd5aa871347b240b43c1f4e2a92c3ee2d4c
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2020-06-29 01:55:12 +0200

    Add show love version in window title

--- conf.lua
+++ conf.lua
@@ -9,7 +9,7 @@ function love.conf(t)
 	t.identity = "picolove"
 	t.version = "0.10.2"
 
-	t.window.title = "picolove 0.1"
+	t.window.title = "picolove 0.1 - (love " .. t.version .. ")"
 	t.window.width = __pico_resolution[1] * scale + xpadding * scale * 2
 	t.window.height = __pico_resolution[2] * scale + ypadding * scale * 2
 	t.window.resizable = true

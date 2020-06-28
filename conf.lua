be7041388ff8a3cdcf475b35830969b5d37b7fc6
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2020-06-29 01:57:26 +0200

    Modify don't show version error love 0.9.2, 0.10.2 and 11.3

--- conf.lua
+++ conf.lua
@@ -7,7 +7,14 @@ function love.conf(t)
 	t.console = true
 
 	t.identity = "picolove"
-	t.version = "0.10.2"
+
+	-- 0.9.2  is wip
+	-- 0.10.2 is default
+	-- 11.3   is wip
+	if t.version ~= "0.9.2" and t.version ~= "0.10.2" and t.version ~= "11.3" then
+		-- show default version if no match
+		t.version = "0.10.2"
+	end
 
 	t.window.title = "picolove 0.1 - (love " .. t.version .. ")"
 	t.window.width = __pico_resolution[1] * scale + xpadding * scale * 2

aa4c61703681cb3d3a4e6c537907291d56722910
Gregor A. Cieslak <gregor.a.cieslak@gmail.com>
2024-03-22 04:46:14 +0100

    Modify implement better version check - fixes #96

--- conf.lua
+++ conf.lua
@@ -5,6 +5,10 @@ xpadding = 8.5
 ypadding = 3.5
 __pico_resolution = { 128, 128 }
 
+local function startsWidth(line, prefix)
+	return string.sub(line, 1, #prefix) == prefix
+end
+
 function love.conf(t)
 	t.console = true
 
@@ -16,18 +20,9 @@ function love.conf(t)
 	-- 0.10.x is default
 	-- 11.x   is wip
 	if
-		t.version ~= "0.9.0"
-		and t.version ~= "0.9.1"
-		and t.version ~= "0.9.2"
-		and t.version ~= "0.10.0"
-		and t.version ~= "0.10.1"
-		and t.version ~= "0.10.2"
-		and t.version ~= "11.0"
-		and t.version ~= "11.1"
-		and t.version ~= "11.2"
-		and t.version ~= "11.3"
-		and t.version ~= "11.4"
-		and t.version ~= "11.5"
+		not startsWidth(t.version, "0.9.")
+		and not startsWidth(t.version, "0.10.")
+		and not startsWidth(t.version, "11.")
 	then
 		-- show default version if no match
 		t.version = "0.10.2"
